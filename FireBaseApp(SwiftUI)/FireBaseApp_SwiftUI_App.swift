49d39ec1be7fc995154618c240990def06b173ea
Katsiaryna Kulik <kulik_03_10@icloud.com>
2026-04-02 15:49:00 +0300

    Initial Commit

---
+++ FireBaseApp(SwiftUI)/FireBaseApp_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseApp_SwiftUI_App.swift
+//  FireBaseApp(SwiftUI)
+//
+//  Created by Екатерина  on 2.04.26.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseApp_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
